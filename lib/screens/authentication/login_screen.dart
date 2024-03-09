5063c2143514dbf359d28acac4185f752120d483
alishaiduc7 <alis.haiduc7773@gmail.com>
2024-03-09 03:24:11 +0200

    Home - merge conflict resolved

--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:it_fest/screens/authentication/_utilities.dart';
